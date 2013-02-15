c6f2eb9678e403de9322c5e1f167efb902de78a6
mrlamroger@gmail.com <mrlamroger@gmail.com>
2013-02-15 01:05:50 -0800

    Initial commit starting over.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Swipemein::Application.config.secret_token = 'ea016bb7fe7fc1e27d9d69ff761e7182db1f12ba1a98db724225a083f8b74e1848dc0517cf43e15d7a6f226a35a0f8bf3d4e56ddabb9a03f7bc2b18c55267394'
